e178b0d05633853fc772509d25a25fb8a68392a8
Alexander Ragland <alexragland2003@gmail.com>
2025-07-08 15:21:38 -0700

    /api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/generate_routine': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/generate_routine/, '/generate_routine')
-      },
-    },
-  },
 })
